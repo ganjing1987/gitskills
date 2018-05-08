2d9b13ad414e13fa11b2d7d551e54891a9f3f943
ganjing <717264162@qq.com>
2018-05-09 00:08:50 +0800

    spring 主方法添加注释

--- spring-boot-activiti1-1/src/main/java/com/example/demo/SpringBootActiviti11Application.java
+++ spring-boot-activiti1-1/src/main/java/com/example/demo/SpringBootActiviti11Application.java
@@ -5,7 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBootActiviti11Application {
-
+   
+	/**spring 主方法 **/
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootActiviti11Application.class, args);
 	}
