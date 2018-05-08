4afa3bf61ee88f37a5081981164573d53e63bab4
ganjing <717264162@qq.com>
2018-05-09 00:43:17 +0800

    branch

--- spring-boot-activiti1-1/src/main/java/com/example/demo/SpringBootActiviti11Application.java
+++ spring-boot-activiti1-1/src/main/java/com/example/demo/SpringBootActiviti11Application.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootActiviti11Application {
    
-	/**spring 主方法 1 **/
+	/**spring 主方法   branch **/
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootActiviti11Application.class, args);
 	}
