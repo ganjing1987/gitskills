aad606e10cf2fb1b2e66fe92e1f1c1fbf5ddfd04
ganjing <717264162@qq.com>
2018-05-10 22:30:10 +0800

    提交git1

--- spring-boot-activiti1-1/src/main/java/com/example/demo/SpringBootActiviti11Application.java
+++ spring-boot-activiti1-1/src/main/java/com/example/demo/SpringBootActiviti11Application.java
@@ -9,6 +9,7 @@ public class SpringBootActiviti11Application {
 
 	/**spring 主方法   master */
 	public static void main(String[] args) {
+		/** 我修改了2 **/
 		SpringApplication.run(SpringBootActiviti11Application.class, args);
 	}
 }
